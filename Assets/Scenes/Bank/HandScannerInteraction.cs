21349d3d8b8d2b9a1e0b49a3158b3d7974513d93
VietNguyen705 <66893867+VietNguyen705@users.noreply.github.com>
2023-04-26 00:27:27 -0700

    bank scene 80%

--- Assets/Scenes/Bank/HandScannerInteraction.cs
+++ Assets/Scenes/Bank/HandScannerInteraction.cs
@@ -5,14 +5,22 @@ public class HandScannerInteraction : MonoBehaviour
 {
     public GameObject player;
     public Animation doorAnimation;
+    public GameObject quizCanvas;
+
+    void Start(){
+                    quizCanvas.SetActive(false);
+
+    }
 
     public void OnHilighted()
-    {
-       
-            
+    {  
             doorAnimation.Play("Open");
 
-       
+    }
+    public void OnHilighted2()
+    {  
+            doorAnimation.Play("Open");
+            quizCanvas.SetActive(true);
     }
 
     
